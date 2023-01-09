512ff20e1c6ef7f12958e0a67361f758a702f5d3
shineHale <3116227229@qq.com>
2023-01-09 18:11:27 +0800

    rewrite fill.asm and submmit the files

---
+++ codes/week4/04/submmit/Mult.asm
@@ -0,0 +1,51 @@
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/04/Mult.asm
+
+// Multiplies R0 and R1 and stores the result in R2.
+// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
+//
+// This program only needs to handle arguments that satisfy
+// R0 >= 0, R1 >= 0, and R0*R1 < 32768.
+
+// Put your code here.
+
+//  i = 1 
+@i
+M = 1
+// product = 0
+@sum
+M = 0
+(LOOP)
+// if ( i > R0 ) goto STOP
+@i
+D = M 
+@R0
+D = D - M 
+@STOP
+D; JGT
+// sum = sum + R1
+@R1
+D = M 
+@sum
+M = M + D 
+// i = i + 1
+@i
+M = M + 1 
+// goto LOOP
+@LOOP
+0;JMP
+
+(STOP) 
+// R2 = sum
+@sum
+D = M 
+@R2 
+M = D 
+
+
+// end of the iteration
+(END)
+	@END
+	0; JMP
