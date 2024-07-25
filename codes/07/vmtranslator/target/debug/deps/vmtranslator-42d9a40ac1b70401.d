f75f8eab5913ca75e8673ebfb782518900ee4892
HaleOIC <shinehale730@gmail.com>
2024-07-25 23:14:01 +0800

    rm some files

--- codes/07/vmtranslator/target/debug/deps/vmtranslator-42d9a40ac1b70401.d
+++
@@ -1,8 +0,0 @@
-/home/hale/Nand2Tetris/codes/07/vmtranslator/target/debug/deps/libvmtranslator-42d9a40ac1b70401.rmeta: src/main.rs src/parser.rs src/code_writer.rs src/cmd_type.rs
-
-/home/hale/Nand2Tetris/codes/07/vmtranslator/target/debug/deps/vmtranslator-42d9a40ac1b70401.d: src/main.rs src/parser.rs src/code_writer.rs src/cmd_type.rs
-
-src/main.rs:
-src/parser.rs:
-src/code_writer.rs:
-src/cmd_type.rs:
